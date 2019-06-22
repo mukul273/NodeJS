1dbff66d3cc03ceb79dca75de09c3aacb5ca3595
Muks <msamak@gmail.com>
2019-06-21 22:30:24 -0400

    Hello World app

---
+++ hello-world/app.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const server = http.createServer((req, res)=> {
+    console.log("Hello");
+    //process.exit();
+});
+const hostname = '127.0.0.1';
+const port = 3000;
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
